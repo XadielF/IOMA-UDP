6ad67c4df905d04e8b232677370a200593482087
leclerc <82549794+XadielF@users.noreply.github.com>
2022-09-15 11:35:41 -0400

    Add files via upload

---
+++ client.py
@@ -0,0 +1,31 @@
+import socket
+import threading
+import random
+
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+#ip = input("Enter server IP address you want to connect: ")
+
+client.bind(("192.168.0.4", random.randint(50000, 60000)))
+
+name = input("Enter username: ")
+
+def receive():
+    while True:
+        try:
+            message, _ = client.recvfrom(1024)
+            print(message.decode())
+        except:
+            pass
+
+t = threading.Thread(target=receive)
+t.start()
+
+client.sendto(f"SIGNUP_TAG:{name}".encode(), ("192.168.0.4", 40000))
+
+while True:
+    message = input("Enter message: ")
+    if message == "exit":
+        exit()
+    else:
+        client.sendto(f"{name}: {message}".encode(), ("192.168.0.4", 40000))
---
+++ server.py
@@ -0,0 +1,42 @@
+import socket
+import threading
+import queue
+
+
+messages = queue.Queue()
+clients = []
+
+#Creating UDP Socket
+server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server.bind(("192.168.0.4", 40000))
+
+def receive():
+    while True:
+        try:
+            message, addr = server.recvfrom(1024)
+            messages.put((message, addr))
+        except:
+            pass
+
+def broadcast():
+    while True:
+        while not messages.empty():
+            message, addr = messages.get()
+            print(message.decode())
+            if addr not in clients:
+                clients.append(addr)
+            for client in clients:
+                try:
+                    if message.decode().startswith("SIGNUP_TAG:"):
+                        name = message.decode()[message.decode().index(":")+1:]
+                        server.sendto(f"{name} joined the server!".encode(), client)
+                    else:
+                        server.sendto(message, client)
+                except:
+                    clients.remove(client)
+
+t1 = threading.Thread(target=receive)
+t2 = threading.Thread(target=broadcast)
+
+t1.start()
+t2.start()
